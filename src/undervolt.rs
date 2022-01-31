a8d085431c0245b857eb8feddc2f2073f95bb55d
Shuzhengz <treez.zhang@gmail.com>
2022-01-31 11:55:41 -0800

    fix type

--- src/undervolt.rs
+++ src/undervolt.rs
@@ -1,20 +1,22 @@
+use phf::phf_map;
+
 use super::config::Config;
 
-pub struct Undervolt {
-}
+pub struct Undervolt {}
 
-let msr_reg = vec![
-    'MSR_PLATFORM_INFO': 0xCE,
-    'MSR_OC_MAILBOX': 0x150,
-    'IA32_PERF_STATUS': 0x198,
-    'IA32_THERM_STATUS': 0x19C,
-    'MSR_TEMPERATURE_TARGET': 0x1A2,
-    'MSR_POWER_CTL': 0x1FC,
-    'MSR_RAPL_POWER_UNIT': 0x606,
-    'MSR_PKG_POWER_LIMIT': 0x610,
-    'MSR_INTEL_PKG_ENERGY_STATUS': 0x611,
-    'MSR_DRAM_ENERGY_STATUS': 0x619,
-    'MSR_PP1_ENERGY_STATUS': 0x641,
-    'MSR_CONFIG_TDP_CONTROL': 0x64B,
-    'IA32_HWP_REQUEST': 0x774,
-];
+const MSR_REG: phf::Map<&'static str, &'static u32> = phf_map! {
+    "MSR_PLATFORM_INFO"         => 0xCE,
+    "MSR_OC_MAILBOX"            => 0x150,
+    "IA32_PERF_STATUS"          => 0x198,
+    "IA32_THERM_STATUS"         => 0x19C,
+    "MSR_TEMPERATURE_TARGET"    => 0x1A2,
+    "MSR_POWER_CTL"             => 0x1FC,
+    "MSR_RAPL_POWER_UNIT"       => 0x606,
+    "MSR_PKG_POWER_LIMIT"       => 0x610,
+    "MSR_PKG_ENERGY_STATUS"     => 0x611,    // Intel Only
+    "MSR_DRAM_ENERGY_STATUS"    => 0x619,
+    "MSR_PP1_ENERGY_STATUS"     => 0x641,
+    "MSR_CONFIG_TDP_CONTROL"    => 0x64B,
+    "IA32_HWP_REQUEST"          => 0x774,
+};
+
